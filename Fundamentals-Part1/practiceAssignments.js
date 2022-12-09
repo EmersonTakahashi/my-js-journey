cfa61b90438a87c1142bcf1cf79afad209f800d9
EmersonTakahashi <tdemerson2000@gmail.com>
2022-12-09 21:25:44 +0000

    Starting the JS journey, fundamentals part 1

---
+++ Fundamentals-Part1/practiceAssignments.js
@@ -0,0 +1,21 @@
+console.log("Assignments start here: ");
+
+let firstName = "Emerson";
+console.log(firstName);
+let myFirstJob = "Backend Developer";
+let myCurrentJob = "Backend Developer";
+let myGoal = "Fullstack Developer";
+
+let country = "Portugal";
+let continent = "Europe";
+let population = 10300000;
+let nationality = "Brazil";
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = "Portuguese";
---
+++ Fundamentals-Part1/script.js
@@ -0,0 +1,85 @@
+//////Variables and Values
+/*let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+//////Variables (values)
+let firstName = "Emerson";
+console.log(firstName);
+let myFirstJob = "Backend Developer";
+let myCurrentJob = "Backend Developer";
+let myGoal = "Fullstack Developer";
+
+let country = "Portugal";
+let continent = "Europe";
+let population = "10.3 millions";
+let nationality = "Brazil";
+
+//Data types
+let ageInt = 23; //number
+let ageDouble = 23.3; //number
+
+let string = "MyString"; //string
+
+let isItRight = false; //bollean
+*/
+
+//////Data types
+//number, string, boolean
+//undefined
+// let javascriptIsFun = true;
+
+// console.log(typeof javascriptIsFun);
+
+// javascriptIsFun = "yes";
+
+// console.log(typeof javascriptIsFun);
+// console.log(javascriptIsFun);
+
+// let year;
+// console.log(typeof year);
+// console.log(year);
+
+// year = 2022;
+
+// console.log(typeof year);
+// console.log(year);
+
+// console.log(typeof null);
+
+/////Let, const, var
+// let age = 30;
+// age = 31;
+
+// const birthYear = 2000;
+//birthYear = 1999; throw error
+
+//var should not be used anymore
+
+//////Operators
+
+// const now = 2022;
+// const ageEmerson = now - 2000;
+// const ageEmilly = now - 2000;
+// console.log(ageEmerson, ageEmilly);
+
+////// Chalenge 1
+//BMI = mass / height ** 2
+const marksWeights = 78;
+const marksHeight = 1.69;
+const johnWeights = 92;
+const johnHeight = 1.95;
+
+const marksWeights2 = 95;
+const marksHeight2 = 1.88;
+const johnWeights2 = 85;
+const johnHeight2 = 1.76;
+
+const marksBMI = marksWeights / marksHeight ** 2;
+const marksBMI2 = marksWeights / marksHeight ** 2;
+const johnBMI = johnWeights / johnHeight ** 2;
+const johnBMI2 = johnWeights2 / johnHeight2 ** 2;
+
+const markHigherBMI = marksBMI > johnBMI;
+const markHigherBMI2 = marksBMI2 > johnBMI2;
+
+console.log(markHigherBMI, markHigherBMI2);
