9ab6d47a1bed8e2ce3b46bd305e20fde8d7b92e9
EmersonTakahashi <tdemerson2000@gmail.com>
2022-12-12 19:26:56 +0000

    function declaration vs expression

--- Fundamentals-Part2/script.js
+++ Fundamentals-Part2/script.js
@@ -13,21 +13,36 @@
 // }
 
 //// function
-function logger() {
-  console.log("My name is Emerson");
-}
-//calling / running / invoking function
-logger();
-logger();
-logger();
-function fruitProcessor(applesQuantity, orangesQuantity) {
-  console.log(applesQuantity, orangesQuantity);
-  const juice = `Juice with ${applesQuantity} apples and ${orangesQuantity} oranges`;
-  return juice;
-}
+// function logger() {
+//   console.log("My name is Emerson");
+// }
+// //calling / running / invoking function
+// logger();
+// logger();
+// logger();
+// function fruitProcessor(applesQuantity, orangesQuantity) {
+//   console.log(applesQuantity, orangesQuantity);
+//   const juice = `Juice with ${applesQuantity} apples and ${orangesQuantity} oranges`;
+//   return juice;
+// }
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
 
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+///// Function declaration vs expressions
+//Function declaration
+function calculateAge1(birthYear) {
+  return 2022 - birthYear;
+}
+const age1 = calculateAge1(2000);
+
+//Function expression (expression produces value)
+const calculateAge2 = function (birthYear) {
+  return 2022 - birthYear;
+};
+const age2 = calculateAge2(2000);
+//We can call function declaration before the declaration but we can't call function expressation before define
+console.log(age1, age2);
