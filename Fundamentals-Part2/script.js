26db6a32ada8f22214dd7b40b4004b804f3090cc
EmersonTakahashi <tdemerson2000@gmail.com>
2022-12-15 18:05:38 +0000

    basic array operations(methods)

--- Fundamentals-Part2/script.js
+++ Fundamentals-Part2/script.js
@@ -62,38 +62,65 @@
 // console.log(yearsUntilRetirement(2000, "Emerson"));
 ////////Array
 
+// const countries = ["Brazil", "Japan", "Portugal", "Denmark"];
+// const years = new Array(2000, 2001, 2002);
+// console.log(countries.length);
+// console.log(countries[countries.length - 1]);
+
+// countries[2] = "Ireland";
+// console.log(countries);
+
+// const firstName = "Emerson";
+// const emerson = [
+//   firstName,
+//   "Takahashi",
+//   2022 - 2000,
+//   "Backend Developer",
+//   countries,
+// ];
+// console.log(emerson);
+
+// //Exercise
+// const calcAge = function (birthYear) {
+//   return 2022 - birthYear;
+// };
+// const yearsToCalculate = [1990, 1967, 2002, 2010, 2018];
+
+// const age1 = calcAge(yearsToCalculate[0]);
+// const age2 = calcAge(yearsToCalculate[1]);
+// const age3 = calcAge(yearsToCalculate[2]);
+// console.log(age1, age2, age3);
+
+// const ages = [
+//   calcAge(yearsToCalculate[0]),
+//   calcAge(yearsToCalculate[1]),
+//   calcAge(yearsToCalculate[2]),
+// ];
+// console.log(ages);
+
+/////Basic Array Operations(Methods)
+//push to add in the end in the array
 const countries = ["Brazil", "Japan", "Portugal", "Denmark"];
-const years = new Array(2000, 2001, 2002);
-console.log(countries.length);
-console.log(countries[countries.length - 1]);
-
-countries[2] = "Ireland";
+const countriesLength = countries.push("Japan");
 console.log(countries);
+console.log(countriesLength);
+//unshift to add in the beggining in the arraty
+countries.unshift("Japan");
+console.log(countries);
+//pop remove the last element in the array
+const removedCountry = countries.pop();
+console.log(countries);
+console.log(removedCountry);
+//shift remove the first element in the array
+countries.shift();
+console.log(countries);
+//indexOf
+console.log(countries.indexOf("Brazil"));
+console.log(countries.indexOf("Italy")); //if doesn't exist, then return -1
+//includes
+console.log(countries.includes("Brazil"));
+console.log(countries.includes("Italy"));
 
-const firstName = "Emerson";
-const emerson = [
-  firstName,
-  "Takahashi",
-  2022 - 2000,
-  "Backend Developer",
-  countries,
-];
-console.log(emerson);
-
-//Exercise
-const calcAge = function (birthYear) {
-  return 2022 - birthYear;
-};
-const yearsToCalculate = [1990, 1967, 2002, 2010, 2018];
-
-const age1 = calcAge(yearsToCalculate[0]);
-const age2 = calcAge(yearsToCalculate[1]);
-const age3 = calcAge(yearsToCalculate[2]);
-console.log(age1, age2, age3);
-
-const ages = [
-  calcAge(yearsToCalculate[0]),
-  calcAge(yearsToCalculate[1]),
-  calcAge(yearsToCalculate[2]),
-];
-console.log(ages);
+if (countries.includes("Brazil")) {
+  console.log("I love Brazil");
+}
