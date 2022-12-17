5c13b7d3df9962ffa219aa55881ef0580528fc34
EmersonTakahashi <tdemerson2000@gmail.com>
2022-12-17 16:25:52 +0000

    challenge 2 using arrays

--- Fundamentals-Part2/challenge.js
+++ Fundamentals-Part2/challenge.js
@@ -1,41 +1,62 @@
-const calcAverage = (scores) => {
-  let sum = 0;
-  for (let i = 0; i < scores.length; i++) {
-    sum += scores[i];
-  }
-  return sum / scores.length;
-};
+///// Challenge 1
 
-function checkWinner(koalasAverage, dolphinsAverage) {
-  if (koalasAverage >= dolphinsAverage * 2) {
-    console.log(`Koalas win (${koalasAverage} vs. ${dolphinsAverage})`);
-  } else if (dolphinsAverage >= koalasAverage * 2) {
-    console.log(`Dolphins win (${dolphinsAverage} vs. ${koalasAverage})`);
+// const calcAverage = (scores) => {
+//   let sum = 0;
+//   for (let i = 0; i < scores.length; i++) {
+//     sum += scores[i];
+//   }
+//   return sum / scores.length;
+// };
+
+// function checkWinner(koalasAverage, dolphinsAverage) {
+//   if (koalasAverage >= dolphinsAverage * 2) {
+//     console.log(`Koalas win (${koalasAverage} vs. ${dolphinsAverage})`);
+//   } else if (dolphinsAverage >= koalasAverage * 2) {
+//     console.log(`Dolphins win (${dolphinsAverage} vs. ${koalasAverage})`);
+//   } else {
+//     console.log("no team wins...");
+//   }
+// }
+
+// let koalasScore = [23, 34, 27];
+// let dolphinsScore = [85, 54, 41];
+
+// let koalasAverage = calcAverage(koalasScore);
+// let dolphinsAverage = calcAverage(dolphinsScore);
+
+// console.log(
+//   `KoalasAverage: ${koalasAverage}`,
+//   `DolphinsAverage: ${dolphinsAverage}`
+// );
+// checkWinner(koalasAverage, dolphinsAverage);
+
+// koalasScore = [65, 54, 49];
+// dolphinsScore = [44, 23, 71];
+
+// koalasAverage = calcAverage(koalasScore);
+// dolphinsAverage = calcAverage(dolphinsScore);
+
+// console.log(
+//   `KoalasAverage: ${koalasAverage}`,
+//   `DolphinsAverage: ${dolphinsAverage}`
+// );
+// checkWinner(koalasAverage, dolphinsAverage);
+
+///// Challenge 2
+function calcTip(billValue) {
+  if (billValue >= 50 && billValue <= 300) {
+    return billValue * 0.15;
   } else {
-    console.log("no team wins...");
+    return billValue * 0.2;
   }
 }
 
-let koalasScore = [23, 34, 27];
-let dolphinsScore = [85, 54, 41];
+// console.log(calcTip(100));
+const bills = [125, 556, 44];
+console.log(bills);
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
 
-let koalasAverage = calcAverage(koalasScore);
-let dolphinsAverage = calcAverage(dolphinsScore);
+const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
 
-console.log(
-  `KoalasAverage: ${koalasAverage}`,
-  `DolphinsAverage: ${dolphinsAverage}`
-);
-checkWinner(koalasAverage, dolphinsAverage);
-
-koalasScore = [65, 54, 49];
-dolphinsScore = [44, 23, 71];
-
-koalasAverage = calcAverage(koalasScore);
-dolphinsAverage = calcAverage(dolphinsScore);
-
-console.log(
-  `KoalasAverage: ${koalasAverage}`,
-  `DolphinsAverage: ${dolphinsAverage}`
-);
-checkWinner(koalasAverage, dolphinsAverage);
+console.log(total);
