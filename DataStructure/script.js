38b50f2fec3fe355bc91d8bc98acbd91a6862c22
EmersonTakahashi <tdemerson2000@gmail.com>
2023-01-08 18:37:38 +0000

    Strings part 1

--- DataStructure/script.js
+++ DataStructure/script.js
@@ -47,40 +47,81 @@ const restaurant = {
   },
 };
 ///////////////////////////////////////
+///////////////Strings/////////////////
+///////////////////////////////////////
+const airline = "TAP Air Portugal";
+const plane = "A320";
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log("B737"[0]);
+console.log(airline.length);
+console.log("B737".length);
+
+console.log(airline.indexOf("r"));
+console.log(airline.lastIndexOf("r"));
+console.log(airline.indexOf("Portugal"));
+console.log(airline.indexOf("portugal")); //-1 case sensitive
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(" ")));
+console.log(airline.slice(airline.lastIndexOf(" ") + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  //B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === "B" || s === "E") {
+    console.log("You got the middle seat :/");
+  } else {
+    console.log("You got lucky :D");
+  }
+};
+checkMiddleSeat("11B");
+checkMiddleSeat("23C");
+checkMiddleSeat("3E");
+
+console.log(new String("Emerson"));
+///////////////////////////////////////
 ///////////Maps Iteration//////////////
 ///////////////////////////////////////
-const question = new Map([
-  ["question", "What is the best programing language in the world?"],
-  [1, "C"],
-  [2, "Java"],
-  [3, "JavaScript"],
-  ["correct", 3],
-  [true, "Correct!"],
-  [false, "Try again..."],
-]);
-console.log(question);
+// const question = new Map([
+//   ["question", "What is the best programing language in the world?"],
+//   [1, "C"],
+//   [2, "Java"],
+//   [3, "JavaScript"],
+//   ["correct", 3],
+//   [true, "Correct!"],
+//   [false, "Try again..."],
+// ]);
+// console.log(question);
 
-//Convert object to map
-console.log(Object.entries(openingHours));
-const hoursMap = new Map(Object.entries(openingHours));
-console.log(hoursMap);
+// //Convert object to map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
 
-//Quiz app
-console.log(question.get("question"));
-for (const [key, value] of question) {
-  if (typeof key === "number") {
-    console.log(`Answer ${key}: ${value}`);
-  }
-}
+// //Quiz app
+// console.log(question.get("question"));
+// for (const [key, value] of question) {
+//   if (typeof key === "number") {
+//     console.log(`Answer ${key}: ${value}`);
+//   }
+// }
 
-const answer = 3; //Number(prompt("Your answer"));
-console.log(answer);
+// const answer = 3; //Number(prompt("Your answer"));
+// console.log(answer);
 
-console.log(question.get(answer === question.get("correct")));
+// console.log(question.get(answer === question.get("correct")));
 
-//Convert map to array
-const questionArray = [...question];
-console.log(questionArray);
+// //Convert map to array
+// const questionArray = [...question];
+// console.log(questionArray);
 
 ///////////////////////////////////////
 /////////Maps fundamentals/////////////
